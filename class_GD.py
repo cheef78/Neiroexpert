526e39b3dcab3812eb89f14e1f2c20b3c7a71785
cheef78 <suslovoleg@mail.ru>
2019-02-23 20:05:39 +0300

    Начало создания класса - Железная дорога

---
+++ class_GD.py
@@ -0,0 +1,11 @@
+class RailWay():
+    def __init__(Plan, Profil, VSP, Zem_Pol, Klimat, Vagon, Poezd):
+        self.Plan = Plan
+        self.Profil = Profil
+        self.VSP = VSP
+        self.Zem_Pol = Zem_Pol
+        self.Klimat = Klimat
+        self.Vagon = Vagon
+        self.Poezd = Poezd
+
+    
