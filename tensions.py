4dd0cb2967f03633033385e776d873aaf8de57e0
cheef78 <suslovoleg@mail.ru>
2020-08-20 23:41:00 +0300

    Начат расчет напряженного состояния пути (рельсы)

--- tensions.py
+++ tensions.py
@@ -1,27 +1,39 @@
-def krom_tension(Q_mean,Q_rms,Y_mean,Y_rms,Kg,Kv,Wg,Wv):
+def krom_tension(input_val):
+
+    q_mean = input_val[0] 
+    q_rms = input_val[1] 
+    Y_mean = input_val[2] 
+    Y_rms = input_val[3] 
+    Kg = input_val[4] 
+    Kv = input_val[5] 
+    Wg = input_val[6] 
+    Wv = input_val[7] 
+    
     '''
-       Функция вычисления кромочных напряжений в рельсах
-       Исходные данные:
-       Q_mean/rms, кН - вертикальная сила (среднее/СКО)
-       Y_mean/rms, кН - боковая сила (среднее/СКО)
-       Kg, cм^-1 - коэффициент отностительной жесткости рельсовой нити в гориз.плоскости
-       Kv, cм^-1 - коэффициент отностительной жесткости рельсовой нити в вертик.плоскости
-       Wg,см^3 - момент сопротивления рельса отностит вертикальной оси для подошвы рельса
-       Wv,см^3 - момент сопротивления рельса отностит горизонтальной оси для подошвы рельса
-       Выходные данные:
-       (mean/sigma)krom_(outer/inner), МПа -среднее значение, СКО кромочных напряжения в наружней/внутренеей кромке
+       'Функция вычисления кромочных напряжений в рельсах
+       'Исходные данные:
+       'fQ_mean/rms, кН - вертикальная сила (среднее/СКО)
+       'Y_mean/rms, кН - боковая сила (среднее/СКО)
+       'Kg, cм^-1 - коэффициент отностительной жесткости рельсовой нити в гориз.плоскости
+       'Kv, cм^-1 - коэффициент отностительной жесткости рельсовой нити в вертик.плоскости
+       'Wg,см^3 - момент сопротивления рельса отностит вертикальной оси для подошвы рельса
+       'Wv,см^3 - момент сопротивления рельса отностит горизонтальной оси для подошвы рельса
+       'Выходные данные:
+       '(mean/sigma)krom_(outer/inner/middle), МПа -среднее значение, СКО кромочных напряжения в наружней/внутренеей кромке/оси рельса
     '''
+    
+    
+    mean_krom_outer = round((10*(q_mean/(4*Kv*Wv))+10*(Y_mean/(4*Kg*Wg))),1)
+    mean_krom_inner = round((10*(q_mean/(4*Kv*Wv))-10*(Y_mean/(4*Kg*Wg))),1)
+    mean_krom_midl = round((10*(q_mean/(4*Kv*Wv))),1)
+    sigma_krom_outer = round((((10*(q_rms/(4*Kv*Wv)))**2+(10*(Y_rms/(4*Kg*Wg)))**2)**0.5),1)
+    sigma_krom_inner = round((((10*(q_rms/(4*Kv*Wv)))**2+(10*(Y_rms/(4*Kg*Wg)))**2)**0.5),1)
+    sigma_krom_midl = round((10*(q_rms/(4*Kv*Wv))),1)
+    return (mean_krom_outer,sigma_krom_outer, mean_krom_inner, sigma_krom_inner, mean_krom_midl, sigma_krom_midl) 
 
-    mean_krom_outer = round((10*(Q_mean/(4*Kv*Wv))+10*(Y_mean/(4*Kg*Wg))),1)
-    mean_krom_inner = round((10*(Q_mean/(4*Kv*Wv))-10*(Y_mean/(4*Kg*Wg))),1)
-    sigma_krom_outer = round((((10*(Q_rms/(4*Kv*Wv)))**2+(10*(Y_rms/(4*Kg*Wg)))**2)**0.5),1)
-    sigma_krom_inner = round((((10*(Q_rms/(4*Kv*Wv)))**2+(10*(Y_rms/(4*Kg*Wg)))**2)**0.5),1)
-    return mean_krom_outer,sigma_krom_outer, mean_krom_inner, sigma_krom_inner
-
-print ("Кромочные напряжения, МПа (среднее/СКО) для наружн и внутр кромок = \n",krom_tension(150,11,100,5.3,0.028,0.01509,75.2,435))
 
 
-def F_shpal(Q_mean,Q_rms,Y_mean,Y_rms,Kg,Kv,l_shp):
+def f_shpal(input):
 
     '''
        Функция вычисления сил, передающихся на шпалы от рельса
@@ -34,15 +46,22 @@ def F_shpal(Q_mean,Q_rms,Y_mean,Y_rms,Kg,Kv,l_shp):
        Выходные данные:
        (mean/sigma) F_shpal_vert/side, кН - среднее значение/СКО сил действующая от рельса на шпалу в вертикальной/горизонтальной плоскости
     '''
+    input[0] = Q_mean
+    input[1] = Q_rms
+    input[2] = Y_mean
+    input[3] = Y_rms
+    input[4] = Kg
+    input[5] = Kv
+    input[6] = l_shp
+    
     mean_F_shpal_vert = round((Q_mean*Kv*l_shp/2),1)
     mean_F_shpal_side = round ((Y_mean*Kg*l_shp/2),1)
     sigma_F_shpal_vert = round((Q_rms*Kv*l_shp/2),1)
     sigma_F_shpal_side = round ((Y_rms*Kg*l_shp/2),1)
     return (mean_F_shpal_vert,sigma_F_shpal_vert, mean_F_shpal_side, sigma_F_shpal_side)
 
-print ("Среднее/СКО значений сил действующаих от рельса на шпалу в вертикальной/горизонтальной плоскости, кН = \n",F_shpal(150,11,100,5.3,0.028,0.01509,50))
 
-def F_ballast (Q_mean,Q_rms, Kv, l_shp, f_shp):
+def f_ballast (input):
     ''' Функция вычисления напряжений, передающихся от подошвы шпалы на балласт
        Исходные данные:
        Q_mean/rms, кН - вертикальная сила (среднее/СКО)
@@ -52,13 +71,18 @@ def F_ballast (Q_mean,Q_rms, Kv, l_shp, f_shp):
        Выходные данные:
        mean/sigma/most_F_ballast, кПа  - среднее/ско/макс.вер напряжения, передающихся от подошвы шпалы на балласт
     '''
+    input[0] = Q_mean
+    input[1] = Q_rms
+    input[2] = Kv
+    input[3] = l_shp
+    input[4] = f_shp
     mean_F_ballast = round((10000*(Q_mean*Kv*l_shp/2)/f_shp),1)
     sigma_F_ballast = round((10000*(Q_rms*Kv*l_shp/2)/f_shp),1)
     most_likely_F_ballast = round((mean_F_ballast+2.5*sigma_F_ballast),1)
-    return (mean_F_ballast,sigma_F_ballast, most_likely_F_ballast)
-print ("Среднее/СКО/Макс.вероятное значения Напряжений, передающиеся от подошвы шпалы на балласт, кПа = \n",F_ballast(150,11,0.01509,50,3092))
+    return (mean_F_ballast,sigma_F_ballast)
 
-def F_ploch (Q_mean,Q_rms, h_ballast, ballast_type):
+def f_ploch (input):
+    
     
     '''
        Функция вычисления напряжений, передающихся на основную площадку земляного полотна
@@ -69,6 +93,10 @@ def F_ploch (Q_mean,Q_rms, h_ballast, ballast_type):
        mean/sigma/most_F_ploch, кПа  - среднее/ско/макс.вер напряжения, передающиеся на основную площадку земляного полотна
 
     '''
+    input[0] = Q_mean
+    input[1] = Q_rms
+    input[2] = h_ballast
+    input[3] = ballast_type
 
     import math
      
@@ -226,8 +254,6 @@ def F_ploch (Q_mean,Q_rms, h_ballast, ballast_type):
     
     most_likely_F_ploch = round ((mean_F_ploch+2.5*rms_F_ploch),1)
     
-    return (mean_F_ploch,rms_F_ploch,most_likely_F_ploch)
-
-print ("Среднее/СКО/Макс.вероятное значения напряжений, передающиеся на основную площадку земляного полотна, кПа = \n",F_ploch(150,15,400,1))
+    return (mean_F_ploch,rms_F_ploch)
 
 
