f329819f20a12158f90f39aa53a6d69bb7acc950
cheef78 <suslovoleg@mail.ru>
2021-03-30 15:35:23 +0300

    продолжаем работу

---
+++ проба сил.py
@@ -0,0 +1,9 @@
+from forces import vagon_1
+from forces import vagon_2
+from forces import vagon_3
+from forces import vagon_4
+from tensions import krom_tension
+from tensions import f_shpal
+from tensions import f_ballast_opzp
+input_val = [0,0,1000,50,60,25,1520,0.25]
+vagon_1 (input_val, 'show')
