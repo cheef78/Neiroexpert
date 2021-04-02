9d441ab74cc99104a044630c3ae1d9807340a0cd
Stepannbt <ignis.stepan@gmail.com>
2021-04-03 01:54:14 +0300

    добавил на кнопку обработку и вызвал ее из питона + повесил на эту кнопку вызов странички с результатом

--- Neiroexpert_WEb/mainapp/views.py
+++ Neiroexpert_WEb/mainapp/views.py
@@ -3,11 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def main (request):
+    name = request.GET.get('name')
+    print(name)
+    click = request.GET.get('click')
+    print(click)
+    if name == 'calculation' and click == 'true':
+        print('Command_read_OK')
     return render(request, 'mainapp/index.html' )
 
 def damage_calculate (request):
+
     result = "Тут отражается результат выполнения функции"
     content = {
-        'fuction_result':result 
+        'fuction_result':result
     }
     return render(request, 'mainapp/ok_form.html', content )
+
+
+
