580130b1db14fb20f543154989bf934dd055e6c8
cheef78 <suslovoleg@mail.ru>
2021-04-04 19:56:43 +0300

    коррекция базы проектов

--- Neiroexpert_WEb/projektapp/models.py
+++ Neiroexpert_WEb/projektapp/models.py
@@ -7,6 +7,6 @@ from django.conf import settings
 class Projekt (models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name= 'projekt' )
     projekt_number = models.PositiveIntegerField(verbose_name= 'номер проекта' , default=1, unique=True, blank = False )
-    description = models.CharField(verbose_name= 'Описание проекта', max_length=255, unique=True, blank = False)
+    description = models.CharField(verbose_name= 'Описание проекта', max_length=255, blank = False, default='жд линия А - Б')
     document = models.FileField(verbose_name= 'файл с исходными данными', upload_to='isxod_dannie/')
     add_datetime = models.DateTimeField(verbose_name= 'время' , auto_now_add= True )
